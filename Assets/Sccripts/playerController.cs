c5c088d6a8dc6b3b2138ef4daee24f7652c0d272
PaReng2 <korreppsj@naver.com>
2025-07-05 19:39:25 +0900

    [Feat] 맵, 플레이어 이동 추가

---
+++ Assets/Sccripts/playerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 8f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3 (moveX, 0, moveY).normalized;
+
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+    }
+}
