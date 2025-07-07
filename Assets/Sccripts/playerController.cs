d3e0b7df63ee8c156471402522eba2bbde89fc78
PaReng2 <korreppsj@naver.com>
2025-07-08 00:42:46 +0900

    [Feat] 탄알, 탄알 스크립트 추가

--- Assets/Sccripts/playerController.cs
+++ Assets/Sccripts/playerController.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    public float moveSpeed = 8f;
+    public float moveSpeed = 4f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,8 +21,13 @@ public class playerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
 
-        Vector3 moveDir = new Vector3 (moveX, 0, moveY).normalized;
+        Vector3 moveDir = new Vector3 (moveX, 0f, moveY);
 
         transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
